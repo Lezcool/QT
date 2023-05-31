dbfdb9100875ae9f2c9dffea5946cc1e4453b122
lezcool <tonyembers@outlook.com>
2023-05-31 15:17:07 +0200

    update

--- code/dl.py
+++ code/dl.py
@@ -7,13 +7,13 @@ today = datetime.date.today()
 
 
 company = ['NVDA','BABA','BILI','DIDIY','COIN','FFIE','META','SPCE','AAPL','NFLX','PDD','NET','NDX']
-data_path = '/home/lez/Documents/QT/QT/data'
+data_path = '/home/ec2-user/aria2-downloads/backtrader/QT/data'
 for c in company:
     #download last day price and add to existing csv file
     # if file doesnt exit, then download
     file = os.path.join(data_path,f'{c}.csv')
     if False:
-        df = pd.read_csv(file,index_col='Date',index_col='Date',parse_dates=True)
+        df = pd.read_csv(file,index_col='Date',index='Date',parse_dates=True)
         last_day_df = pd.DataFrame(yf.Ticker(c).history().iloc[-1,:5]).T
         if last_day_df.index[-1].date() != str(df.index[-1]):
             new_df = pd.concat([df,last_day_df])
